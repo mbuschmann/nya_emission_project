b04b96d390e81b30c602f6f4d50f53f51283eff8
Mathias Palm <mathias@iup.uni-bremen.de>
2020-04-30 11:06:51 +0200

    Modifications to make opus script work and create the list

--- opus/make_list.py
+++ opus/make_list.py
@@ -6,40 +6,101 @@ import numpy as np
 import sys
 
 
-def make_list(path):
+def make_list(path,ldate,tpath=''):
 
-    files = os.listdir(path) 
 
+    if len(tpath) == 0:
+        tpath = path
     atmo = []
     cold = []
     bb = []
     hot = []
 
-    # Construct name of list file
-    lfile = '{}{}'.format(path.split(os.path.sep)[-1],'.list')
+    #scold = 'sr80'
+    #scold = 'rt'
+    scold = 'bb_20.0'
+    #shot = 'ir301'
+    #shot = 'ht1'
+    shot = 'bb_100.0'
+    ssky = 'roof'
+    # ssky = 'up'
+    # ssky = 'ir301'
+    scold = 'sr800_20.0'
+    shot = 'sr800_110.0'
+    scold2 = 'sr800_19.9'
+    shot2 = 'sr800_109.9'
+
+    files = os.listdir(os.path.join(path,'Emission',ldate))
+    lfile = '{}{}'.format(ldate,'.list')
     
     fid = open(lfile,'w')
     for ff in files:
-        if ff.find('bb_20.0') > -1:
-            cold.append([ff, datetime.strptime(ff[4:18],'%Y%m%d%H%M%S')])
+        if ff.find('_%s'%scold) > -1 or ff.find('_%s'%scold2) > -1:
+            cold.append([ff, datetime.strptime(ff[4:18],'%Y%m%d%H%M%S'),20])
             cold.sort()
-        if ff.find('bb_100.0') > -1:
-            hot.append([ff,datetime.strptime(ff[4:18],'%Y%m%d%H%M%S')])
+        if ff.find('_%s'%shot) > -1 or ff.find('_%s'%shot2) > -1:
+            hot.append([ff,datetime.strptime(ff[4:18],'%Y%m%d%H%M%S'),110])
             hot.sort()
-        if ff.find('_up.') > -1:
+        if ff.find('_%s'%ssky) > -1:
             atmo.append([ff,datetime.strptime(ff[4:18],'%Y%m%d%H%M%S')])
             atmo.sort()
 
+    td = lambda x: datetime.strptime(path+' '+x, '%Y%m%d %H:%M:%S')
+    
+    ht1 = []
+    rt = []
+    dnum1 = []
 
+    if False:
+        with open(os.path.join(path,'Emission','tlogs',ldate+'_bb_targets.dat')) as tfid:
+            for ll in tfid:
+                en = ll.split()
+                if len(en)<11:
+                    continue
+                try:
+                    dnum1.append(td(en[1]))
+                except:
+                    continue
+                ht1.append(np.mean(list(map(float,en[2:5]))))
+                rt.append(np.mean(list(map(float,en[7:10]))))
+
+    sr80 = []
+    dnum2 = []
+    td = lambda x: datetime.strptime(x, '%Y%m%d %H:%M:%S')
+    if False:
+        tfid = open(path+'tlogs/'+ldate+'_sr80_targets.dat')
+        tfid.seek(0)
+        for i in range(0,7):
+            tfid.readline()
+        for ll in tfid:
+            en = ll.split()
+            #            try:
+            sr80.append([en[2], td(en[0]+' '+en[1])])
+            #            except:
+            #                continue
+            
+        tfid.close()
+    if scold == 'sr80':
+        for cind in range(0,len(sr80)):
+            ind = np.where(sr80[cind][1] == np.array(cold)[:,1])
+            if ind[0].size>0:
+                i = ind[0][0]
+                print(cold[i], sr80[cind][0])
+                cold[i][2] = sr80[cind][0]
+                fid.write('{}\n'.format(len(atmo)))
     fid.write('{}\n'.format(len(atmo)))
     for at in atmo:
-        # find nearest cold spectrum
+	# find nearest cold spectrum
         ic = np.argmin(np.abs(at[1] - np.array(cold)[:,1]))
         # find nearest hot spectrum
         ih = np.argmin(np.abs(at[1] - np.array(hot)[:,1]))
         # create name of final spectrum
         final = '{}{}{}'.format(at[0].split('_')[0],'_FINAL.',at[0].split('.')[-1])
-        fid.write('{:<100}{:<50}{:<50}{:<50}{:<50}{:<10}{:<10}\n'.format(path,hot[ih][0],cold[ih][0], at[0], final, 373.15, 293.15))
+        if tpath.find('\\'):
+            sep = r'\\'
+        else:
+            sep = '/'
+        fid.write('{:<100}{:<50}{:<50}{:<50}{:<50}{:<10}{:<10}\n'.format(sep.join((tpath,'Emission',ldate)),hot[ih][0],cold[ic][0], at[0], final, float(hot[ih][2]) + 273.15, float(cold[ic][2]) + 273.15))
 
     fid.close()
 
@@ -48,9 +109,12 @@ if __name__ == '__main__':
     #path = '/home/mathias/Vertex80_spectren/20190829'
 
     path = sys.argv[1]
-    if len(sys.argv) == 2:
-        make_list(path)
+    ldate = sys.argv[2]
+    if len(sys.argv) == 4:
+        make_list(path, ldate, sys.argv[3])
+    elif len(sys.argv) == 3:
+        make_list(path, ldate)
     else:
-        print('Call as python3 make_list.py path')
+        print('Call as python3 make_list.py path date(YYYYMMDD) [target path]')
     
 
